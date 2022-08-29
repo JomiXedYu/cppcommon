6c18c6c729a1e27e32d23d2831ab1143e2128325
雪千渔 <JomiXedYu@outlook.com>
2022-08-29 13:43:20 +0800

    update

---
+++ CppTest/CppTest/CppTest.cpp
@@ -0,0 +1,15 @@
+// CppTest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+    unsigned short s = L'啊';
+    std::cout << s;
+
+}
